47d4462193b94304766cd967d48d88a7c73bd68d
Andy Leekung <chingyeuleekung@gmail.com>
2018-09-29 17:01:32 -0400

    Initial commit

---
+++ app/src/main/java/com/google/firebase/example/takecare/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,36 @@
+package com.google.firebase.example.takecare.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.google.firebase.example.takecare.Filters;
+
+/**
+ * ViewModel for {@link com.google.firebase.example.takecare.MainActivity}.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+    private Filters mFilters;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+        mFilters = Filters.getDefault();
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
+    public Filters getFilters() {
+        return mFilters;
+    }
+
+    public void setFilters(Filters mFilters) {
+        this.mFilters = mFilters;
+    }
+}
